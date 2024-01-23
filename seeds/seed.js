eeaa1c9c44945f127a94936dea44ab17e6496310
ethanfrog <101209254+ethanwynne395@users.noreply.github.com>
2024-01-22 23:08:24 -0500

    Added README file

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,7 +3,6 @@ const { User } = require('../models');
 
 const userData = require('./userData.json');
 
-
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
@@ -11,8 +10,6 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
-
- 
 
   process.exit(0);
 };
